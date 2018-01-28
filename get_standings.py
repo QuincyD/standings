b83387f9dda9eb4ac605f67b10f378ed2753c02e
QuincyD <quincyd@umich.edu>
2018-01-27 18:09:56 -0600

    Initial commit for NBA standings converter
    Create html index page to ultimately display the conference
    converted standings.
    
    Also, create python file to get the current NBA standings.
    The python file visits two json urls to get teams.json and
    standings.json. It then saves them to json files and saves
    the current date and time to last_updated.json.

---
+++ get_standings.py
@@ -0,0 +1,65 @@
+'''
+Programmed by Quincy Davenport
+
+Program scans the urls for teams and standings json
+objects to retrieve the NBA standings and saves them to
+json files; teams.json and standings.json.
+
+It also saves the current data and time to last_updated.json.
+'''
+
+import requests
+import json
+import re
+from datetime import datetime
+
+'''
+EFFECTS:	Scans the json_url url and saves its contents
+			to file output_filename
+'''
+def getJSON(json_url, output_filename):
+	
+	session = requests.Session()
+	try:
+		response = session.get(json_url, timeout=10)
+	except:
+		return
+
+	json_data = json.loads(response.text.encode('UTF-8').decode('ascii', 'ignore'))
+	with open(output_filename, 'w') as json_file:
+		json.dump(json_data, json_file)
+
+
+'''
+EFFECTS:	Adds the date and time to a dict and saves it to
+			json file output_filename
+'''
+def getTime(output_filename='last_updated.json'):
+
+	time_str = str(datetime.now())
+
+	year = time_str[:4]
+	month = time_str[5:7]
+	day = time_str[8:10]
+	date = month + '/' + day + '/' + year
+
+	hour = int(time_str[11:13])
+	am_pm = 'am'
+	if (hour >= 12):
+		hour -= 12
+		hour = str(hour)
+		am_pm = 'pm'
+	minute = time_str[14:16]
+	time = hour + ':' + minute + ' ' + am_pm 
+
+	time_dict = {'date':date, 'time':time}
+
+	with open(output_filename, 'w') as json_file:
+		json.dump(time_dict, json_file)
+
+
+if __name__ == '__main__':
+
+	getJSON('https://data.nba.net/prod/v1/2017/teams.json', 'teams.json')
+	getJSON('https://data.nba.net/prod/v1/current/standings_conference.json', 'standings.json')
+	getTime()
