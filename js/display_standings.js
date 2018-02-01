47068f708cf6763a89ce2f3807b3d0a45bcb56fc
QuincyD <quincyd@umich.edu>
2018-02-01 14:07:21 -0600

    Add color addition to dynamic tables
    Add table colors dynamically with display_standings.js
    Also, update code in index.css and index.html to
    utilizes the additions.

--- js/display_standings.js
+++ js/display_standings.js
@@ -1,20 +1,62 @@
+colors = [];
+colors[0] = {'color':'blue', 'back':'#ddf'};
+colors[1] = {'color':'green', 'back':'#dfd'};
+colors[2] = {'color':'red', 'back':'#fdd'};
+colors[3] = {'color':'orange', 'back':'#fda'};
+colors[4] = {'color':'purple', 'back':'#ecf'};
+colors[5] = {'color':'yellow', 'back':'#ffd'};
+colors[6] = {'color':'white', 'back':'#eee'};
+colors[7] = {'color':'#444', 'back':'#ccc'};
+
 
 function displayStanding(standings){
 
     var last_updated = parseLastUpdated();
+    var sorted_confs = [];
+    for (var conf in standings) {
+        sorted_confs.push(conf); 
+    }
+    sorted_confs.sort();
 
-    for (var conf in standings){
+    var k = 0;
+    for (var i = 0; i < sorted_confs.length; i++){
 
         conferences = document.getElementById('conferences');
 
+        conf = sorted_confs[i];
         var conf_name = conf.charAt(0).toUpperCase() + conf.slice(1);
 
-        table = '<h2 id=\'' + conf +
+        var table = '<h2 id=\'' + conf +
                 '_title\' class=\'conf_title\'>' +
                 conf_name + ' Conference</h2>';
         table += '<table id=\'' + conf + '\'></table>';
 
         conferences.innerHTML += table;
+
+        // Addition of table colors ----------------------------
+        var conf_colors = document.getElementById('conf_colors');
+
+        var title_color = '#' + conf + '_title {';
+        title_color += 'background-color: ' + colors[k]['color'] +
+                        ';';
+        title_color += '}';
+
+        var table_colors = '#' + conf + ', #' + conf + ' th {';
+        table_colors += 'background-color: ' + colors[k]['back'] +
+                        ';';
+        table_colors += 'border: 3px solid ' + colors[k]['color'] +
+                        ';';
+        table_colors += 'border-top: none;';
+        table_colors += '}';
+        k++;
+
+        if (k >= colors.length) {
+            k = 0;
+        }
+
+        conf_colors.innerHTML += title_color;
+        conf_colors.innerHTML += table_colors;
+        //~Addition of table colors ---------------------------
 
         conf_table = document.getElementById(conf);
 
