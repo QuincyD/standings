6793fffc8d354374288dadb0f2de03cee33d8e5d
QuincyD <quincyd@umich.edu>
2018-02-03 13:55:32 -0600

    Readability, style, and speed update
    Make code more readable using loops on repetitious html
    and by breaking up long lines into multiple lines.
    Update page style. Improve page speed by accesing pages'
    DOM for writing far less with temp strings.

--- js/create.js
+++ js/create.js
@@ -1,12 +1,12 @@
 temp_names = [];
-temp_names[0] = 'Sky';
-temp_names[1] = 'Forest';
-temp_names[2] = 'Volcano';
-temp_names[3] = 'Equator';
-temp_names[4] = 'Wild';
-temp_names[5] = 'Sun';
-temp_names[6] = 'Artic';
-temp_names[7] = 'Shadow';
+temp_names[0] = 'Eastern';
+temp_names[1] = 'Western';
+temp_names[2] = 'Northern';
+temp_names[3] = 'Southern';
+temp_names[4] = 'North Eastern';
+temp_names[5] = 'North Western';
+temp_names[6] = 'South Eastern';
+temp_names[7] = 'South Western';
 
 colors = [];
 colors[0] = {'color':'blue', 'back':'#ddf'};
@@ -29,7 +29,9 @@ $.ajax({
 
 	    var temp_arr = data['league']['standard'];
 
-	    for (var i = 0; i < temp_arr.length; i++){
+        var i;
+        var temp_len = temp_arr.length;
+	    for (var i = 0; i < temp_len; i++){
 
 	    	var team = temp_arr[i];
 
@@ -56,17 +58,20 @@ $.ajax({
 function newNums() {
 
     var nums = document.getElementById('conference_num');
+    nums.innerHTML = '';
 
-    nums.innerHTML = '<p>Choose number of converences</p>';
-    nums.innerHTML += '<form id=\'num_form\' class=\'line\'>';
-    nums.innerHTML += '<input id=\'2\' class=\'radio_btn\' type=\'radio\' name=\'num_confs\' onclick=showNumButton(2); return false;>2';
-    nums.innerHTML += '<input id=\'3\' class=\'radio_btn\' type=\'radio\' name=\'num_confs\' onclick=showNumButton(3); return false;>3';
-    nums.innerHTML += '<input id=\'4\' class=\'radio_btn\' type=\'radio\' name=\'num_confs\' onclick=showNumButton(4); return false;>4';
-    nums.innerHTML += '<input id=\'5\' class=\'radio_btn\' type=\'radio\' name=\'num_confs\' onclick=showNumButton(5); return false;>5';
-    nums.innerHTML += '<input id=\'6\' class=\'radio_btn\' type=\'radio\' name=\'num_confs\' onclick=showNumButton(6); return false;>6';
-    nums.innerHTML += '<input id=\'7\' class=\'radio_btn\' type=\'radio\' name=\'num_confs\' onclick=showNumButton(7); return false;>7';
-    nums.innerHTML += '<input id=\'8\' class=\'radio_btn\' type=\'radio\' name=\'num_confs\' onclick=showNumButton(8); return false;>8';
-    nums.innerHTML += '</form>';
+    var temp_text = '<p>Choose number of converences</p>';
+    temp_text += '<form id=\'num_form\'>';
+
+    for (var i = 2; i <= 8; i++) {
+        temp_text += '<label><input id=\'' + i +
+        '\' class=\'radio_btn\' type=\'radio\' onclick=showNumButton(' +
+        i + ');return false;>' + i + '</label>';
+    }
+
+    temp_text += '</form>';
+    
+    nums.innerHTML += temp_text;
 
 }
 
@@ -87,7 +92,7 @@ function showNumButton(num) {
     }
     
     document.getElementById('conference_num').innerHTML +=
-    '<button id=\'num_btn\' class=\'line\' onclick=\'nameConferences();return false;\'>OK!</button>';
+    '<button id=\'num_btn\' onclick=\'nameConferences();return false;\'>OK!</button>';
 
     document.getElementById(num).checked = true;
 }
@@ -96,34 +101,37 @@ function nameConferences() {
     
     document.getElementById('conference_num').innerHTML = ''; 
 
-    conferences = document.getElementById('conference_names');
-    conferences.innerHTML = '<p>Name your conferences</p>';
+    var conferences = document.getElementById('conference_names');
+    var temp_text = '<p>Name your conferences</p>';
 
     for (var i = 0; i < clicked_num; i++) {
-        var conf = '<input class=\'color_' + i + '\' type=\'text\' id=\'conf_' +
-                    i + '_name\' value=\'' + temp_names[i] +
-                    '\'>&nbsp;Conference<br>'; 
-        conferences.innerHTML += conf;
+        temp_text += '<input class=\'color_' + i + '\' ';
+        temp_text += 'type=\'text\' name=\'conf_';
+        temp_text += 'name\' value=\'' + temp_names[i];
+        temp_text += '\'>&nbsp;Conference<br>'; 
     }
-    conferences.innerHTML +=
-    '<button onclick=\'chooseConferences();return false;\'>OK!!</button>';
+    temp_text += '<button onclick=\'chooseConferences();';
+    temp_text += 'return false;\'>OK!!</button>';
+
+    conferences.innerHTML = temp_text;
 }
 
 function chooseConferences() {
 
-    given_names = [];
-    var count = 0;
-    while(conference_name =
-            document.getElementById('conf_' + count + '_name')) {
+    var conf_names = document.getElementsByName('conf_name');
 
-        given_names.push(conference_name.value);
-        count++;
+    given_names = [];
+
+    var i;
+    var conf_len = conf_names.length;
+    for (i = 0; i < conf_len; i++) {
+        given_names.push(conf_names[i].value);
     }
+    given_names.sort();
 
     document.getElementById('conference_names').innerHTML = '';
     
     displayChoices();
-
 }
 
 function displayChoices() {
@@ -131,29 +139,43 @@ function displayChoices() {
     conference_choices = document.getElementById('conference_choices');
     chosen = 0;
 
-    for (var i = 0; i < teams.length; i++) {
+    var temp_text = '<p>Choose conference for each team</p><br>';
+
+    var i;
+    var teams_len = teams.length;
+    for (i = 0; i < teams_len; i++) {
 
         var name = teams[i]['name'];
         var team_id = teams[i]['team_id'];
 
-        conference_choices.innerHTML += '<img src=\'../imgs/logos/' +
-                                        team_id + '.png\' class=\'line\'>';
-        conference_choices.innerHTML += '&nbsp;<p class=\'line\'>' + name + ': '; 
-        conference_choices.innerHTML += '<br>';
+        temp_text += '<img src=\'../imgs/logos/' + team_id;
+        temp_text += '.png\' class=\'line\'>';
+        temp_text += '&nbsp;<p class=\'line\'>' + name + ': '; 
+        temp_text += '<br>';
 
-        conference_choices.innerHTML += '<form class=\'choices\'>';
-        for (var j = 0; j < given_names.length; j++) {
+        temp_text += '<form class=\'choices\'>';
+
+        var j;
+        var given_len = given_names.length;
+        for (j = 0; j < given_len; j++) {
 
             var name = given_names[j];
 
-            conference_choices.innerHTML +=
-                '<input class=\'color_' + j + '\' class=\'radio_btns\' type=\'radio\' onclick=\'$(this).attr("checked", true); chooseConference(' + i + ', ' + j + '); return false;\'>' + name;
+            temp_text += '<button class=\'color_' + j;
+            temp_text += '\' onclick';
+            temp_text += '=\'chooseConference(' + i + ', ' + j;
+            temp_text += '); return false;\'>' + name + '</button>';
+
+            if (j === 3 && 4 !== given_len) {
+                temp_text += '<br>';
+            }
 
         }
-        conference_choices.innerHTML += '</form>';
-
-        conference_choices.innerHTML += '</p><br>';
+        
+        temp_text += '</form></p><br>';
     }
+
+    conference_choices.innerHTML = temp_text;
 }
 
 function chooseConference(team_num, conf_num) {
